4124f1f89e861507fc21cb084ed18e8bac7758ae
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-01-06 19:53:00 +0200

    Hiding secret key

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nvuqb64lfp5)68$a0nh$)anz@l1q&gk12rm4xxsnd22ih*1puy'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
