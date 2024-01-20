e0e4aa946b338969596783f18ca88887d11d0b6c
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-01-20 15:49:06 +0200

    Changing wsgi

--- restaurant_kitchen_service/wsgi.py
+++ restaurant_kitchen_service/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_kitchen_service.settings')
+path = 'kitchen'
+if path not in sys.path:
+    sys.path.append(path)
 
 application = get_wsgi_application()
